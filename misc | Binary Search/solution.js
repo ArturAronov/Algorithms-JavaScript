42f38c13c7cac31301569045bcbe4a0e5c83ea05
Artur Aronov <unionco@icloud.com>
2021-11-21 00:56:24 +0800

    changed the formatting

---
+++ misc | Binary Search/solution.js
@@ -0,0 +1,18 @@
+const binarySearch=function(arr, search){
+  let lowerBound=0;
+  let upperBound=arr.length-1;
+  while(lowerBound<=upperBound){
+    const midpoint=parseInt((upperBound+lowerBound)/2);
+    const valueAtMidpoint=arr[midpoint];
+    if(search===valueAtMidpoint){
+      return `Number ${search} is located in the index ${midpoint}.`;
+    }else if(search<valueAtMidpoint){
+      upperBound=Math.floor(midpoint-1);
+    }else if(search>valueAtMidpoint){
+      lowerBound=Math.floor(midpoint+1);
+    };
+  };
+  return `Number ${search} is not found in the array.`;
+};
+
+console.log(binarySearch([2,33,56,79,88,103,200,666], 88));
