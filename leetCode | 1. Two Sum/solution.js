4c4e4fef629c5f382d5aa092771ea4a2ff9e43da
Artur Aronov <unionco@icloud.com>
2021-12-15 01:16:59 +0800

    changed algorithm

--- leetCode | 1. Two Sum/solution.js
+++ leetCode | 1. Two Sum/solution.js
@@ -2,9 +2,7 @@ var twoSum = function(nums, target) {
   const result=[];
   for(let i=0; i<nums.length; i++){
     for(let j=i+1; j<nums.length; j++){
-      if(i===j){
-        j++;
-      }else if(nums[i]+nums[j]===target){
+      if(nums[i]+nums[j]===target){
         result.push(i,j);
       };
     };
